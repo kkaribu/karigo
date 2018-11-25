503ee4faeb818ccc6a69ef73b283b78895ff055d
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-11-24 20:42:54 -0500

    Add Key struct

---
+++ key.go
@@ -0,0 +1,14 @@
+package karigo
+
+// Key ...
+type Key struct {
+	Type  string
+	ID    string
+	Field string
+}
+
+// String ...
+// TODO Implement Key.String
+func (k *Key) String() string {
+	return ""
+}
